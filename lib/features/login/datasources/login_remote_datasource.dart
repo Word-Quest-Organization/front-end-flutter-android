ff7024d7c99e60d52bdbed0a032e288a8c2d42b3
NatanaelHLacerda <natanaelhlacerda@gmail.com>
2025-10-29 18:24:45 -0300

    update Request Login Api

--- lib/features/login/datasources/login_remote_datasource.dart
+++ lib/features/login/datasources/login_remote_datasource.dart
@@ -1,22 +1,37 @@
+import 'dart:convert';
+
 import 'package:english_app/core/result/failure.dart';
-import 'package:english_app/features/login/data/models/user_model.dart';
 import 'package:english_app/features/login/domain/entities/params.dart';
+import 'package:http/http.dart' as http;
 
 abstract class LoginRemoteDatasource {
-  Future<UserModel> login(LoginParams params);
+  Future<String> login(LoginParams params);
 }
 
 class LoginRemoteDatasourceImpl implements LoginRemoteDatasource {
+  final http.Client client;
+  final String baseUrl;
+  LoginRemoteDatasourceImpl({required this.client, required this.baseUrl});
   @override
-  Future<UserModel> login(LoginParams params) async {
-    // Simulate a network call with a delay
-    await Future.delayed(Duration(seconds: 1));
-    if (params.username == 'teste@teste.com') {
-      // Simula um JSON de resposta
-      final fakeJson = {'id': '1', 'name': 'Usuário Teste', 'email': params.username};
-      return UserModel.fromJson(fakeJson);
-    } else {
-      throw ServerFailure("E-mail ou senha inválidos");
+  Future<String> login(LoginParams params) async {
+    final url = Uri.parse('$baseUrl/auth/login');
+
+    try {
+      final response = await client.post(
+        url,
+        headers: {'Content-Type': 'application/json'},
+        body: json.encode({
+          'username': params.username,
+          'password': params.password,
+        }),
+      );
+      if (response.statusCode == 200) {
+        return json.decode(response.body)['token'];
+      } else {
+        throw ServerFailure('Failed to login: ${response.statusCode}');
+      }
+    } catch (e) {
+      throw ServerFailure('Failed to login: $e');
     }
   }
 }
